038f6060bbbdae532ca732a084f6b3c5a00ea8f2
Rahul Aedula <rahulaedula95@gmail.com>
2020-06-26 23:48:26 -0600

    Reverted back to author's code and reduced the data permutation from 20 to 1 to save time git add features_extract.py

--- 104/features_extract.py
+++ 104/features_extract.py
@@ -7,6 +7,7 @@
 
 
 import pandas as pd
+import scipy
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.dummy import DummyRegressor
@@ -26,7 +27,8 @@ from mmltoolkit.descriptors import *
 from mmltoolkit.featurizations import *
 from sklearn.model_selection import RandomizedSearchCV as CV
 
-
+def mean_absolute_error(y_true, y_pred):
+    return np.mean(np.abs((y_true - y_pred)))
 def make_score_dict(x,y,krr):
 
     
@@ -35,7 +37,7 @@ def make_score_dict(x,y,krr):
     
 
     keys = ['mean_test_MAE', 'mean_train_MAE', 'std_test_MAE','mean_test_r2', 'mean_train_r2', 'std_test_r2','mean_test_p', 'mean_train_p', 'std_test_p']
-    return ({x:np.mean(clf.cv_results_[x]) for x in keys})
+    return ({x:clf.cv_results_[x] for x in keys})
 
 def pearson(y_true,y_pred):
     return (np.corrcoef(y_true, y_pred)[0, 1])
@@ -45,7 +47,7 @@ def mean_absolute_percentage_error(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / (y_true))) * 100
 
 
-def all_test_everything(data, featurization_dict, targets, inner_cv=KFold(n_splits=2,shuffle=True),
+def all_test_everything(data, featurization_dict, targets, inner_cv=KFold(n_splits=5,shuffle=True),
                     outer_cv=ShuffleSplit(n_splits=1, test_size=0.2), verbose=False, normalize=False ):
     '''
         test all combinations of target variable, featurizations, and models
@@ -91,7 +93,7 @@ def all_test_everything(data, featurization_dict, targets, inner_cv=KFold(n_spli
             if (x.ndim == 1):
                 x = x.reshape(-1,1)
             
-            print (x)
+            #print (x)
             if (normalize):
                 st = StandardScaler()
                 x = st.fit_transform(x)
@@ -105,26 +107,32 @@ def all_test_everything(data, featurization_dict, targets, inner_cv=KFold(n_spli
                 model = model_dict[modelname]['model']
                 param_grid = model_dict[modelname]['param_grid']
 
-                # scores_dict = nested_grid_search_CV(x, y, model, param_grid,
-                #                                     inner_cv=KFold(n_splits=2, shuffle=True),
-                #                                     outer_cv=outer_cv, verbose=verbose)
+                scores_dict = nested_grid_search_CV(x, y, model, param_grid,
+                                                    inner_cv=KFold(n_splits=5, shuffle=True),
+                                                    outer_cv=outer_cv, verbose=verbose)
                 # print ("Stuck")
 
-                scoring = {'MAE':'neg_mean_absolute_error', 'r2':'r2','p':make_scorer(pearson),'MAPE':make_scorer(mean_absolute_percentage_error)}
-                krr = CV(KernelRidge(kernel='rbf'), param_distributions=param_grid, cv=20, scoring=scoring, n_iter=5, n_jobs=-1, refit='MAE', return_train_score=True)
-                
-                scores_dict = make_score_dict(x,y,krr)
-                modelresults[modelname] = scores_dict['mean_test_MAE']
 
-                if (scores_dict['mean_test_MAE'] < best_value):
+
+                # a = 1
+                # g = 0.01
+                # #eta = 0.01
+                # param_grid = {'alpha': scipy.stats.expon(scale=a), 'gamma': scipy.stats.expon(scale=g)}
+                # scoring = {'MAE':make_scorer(mean_absolute_error), 'r2':'r2','p':make_scorer(pearson),'MAPE':make_scorer(mean_absolute_percentage_error)}
+                # krr = CV(KernelRidge(kernel='rbf'), param_distributions=param_grid, cv=5, scoring=scoring, n_iter=1, n_jobs=-1, refit='MAE', return_train_score=True, random_state=0,iid=False)
+                
+                #scores_dict = make_score_dict(x,y,krr)
+                modelresults[modelname] = scores_dict
+
+                if (scores_dict['MAE'] < best_value):
                     best[target]=[featurization, modelname]
-                    best_value = scores_dict["mean_test_MAE"]
+                    best_value = scores_dict["MAE"]
 
             featurizationresults[featurization] = modelresults
         
         results[target] = featurizationresults
 
-    return (dict(results), dict(best))
+    return (dict(results), dict(best),scores_dict)
 
 if __name__ == "__main__":
 
@@ -246,7 +254,7 @@ if __name__ == "__main__":
     ]
 
     
-    (results, best) = all_test_everything(data, featurization_dict, targets, verbose=True, normalize=True )
+    (results, best,fin) = all_test_everything(data, featurization_dict, targets, verbose=True, normalize=True )
     with warnings.catch_warnings():
         warnings.filterwarnings('ignore',r'LinAlgWarning')
 
@@ -254,3 +262,4 @@ if __name__ == "__main__":
 
     pickle.dump( results, open( "data/test_all_results3.pkl", "wb" ) )
     pickle.dump( best, open( "data/test_all_best3.pkl", "wb" ) )
+    pickle.dump( fin, open( "data/test_all_fin3.pkl", "wb" ) )
